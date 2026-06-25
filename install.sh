2530180f5139ad6a7110ef80802fdd3021d759cf
napNsleep <245271671+napNsleep@users.noreply.github.com>
2026-06-24 20:49:29 -0500

    fixes script variable

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-EXT_PATH=$(mkdir -p $HOME/.local/share/nautilus-python/extensions)
+mkdir -p $HOME/.local/share/nautilus-python/extensions
+EXT_PATH=$HOME/.local/share/nautilus-python/extensions
 
 cp ./edit_file.py $EXT_PATH
 cp ./open_in_terminal.py $EXT_PATH
