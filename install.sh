da9d667a2424fc910fe104a87d473d022cff984c
napNsleep <245271671+napNsleep@users.noreply.github.com>
2026-06-24 20:40:45 -0500

    fixes variable name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXT_PATH="$HOME/.local/share/nautilus-python/extensions"
+EXT_PATH=$(mkdir -p $HOME/.local/share/nautilus-python/extensions)
 
 cp ./edit_file.py $EXT_PATH
 cp ./open_in_terminal.py $EXT_PATH
